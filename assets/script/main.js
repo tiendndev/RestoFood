e730c4fbf774e180c5460a1dfabaf1ad6c658757
tienlaptrinh <tienlaptrinh>
2022-09-23 23:33:03 +0700

    push code

--- assets/script/main.js
+++ assets/script/main.js
@@ -32,3 +32,24 @@ var swiper = new Swiper(".home-slider", {
     },
     loop:true,
 });
+
+var swiper = new Swiper(".review-slider", {
+    spaceBetween: 20,
+    centeredSlides: true,
+    autoplay: {
+      delay: 7500,
+      disableOnInteraction: false,
+    },
+    loop:true,
+    breakpoints: {
+        0: {
+            slidesPerView: 1,
+        },
+        740: {
+            slidesPerView: 2,
+        },
+        1024: {
+            slidesPerView: 3,
+        }
+    }
+});
